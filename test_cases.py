d9ddaf510ed43d738f429ce24a321b43a2f13cb5
Togo555-mp4 <5t1ng20X@gmail.com>
2025-02-08 17:45:34 +0900

    テストケースの変更

--- test_cases.py
+++ test_cases.py
@@ -7,15 +7,56 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+    # 有効同値
+    def test_valid_input_min(self):
+        self.assertEqual(calc(1, 1), 1)
 
-        def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+    def test_valid_input_max(self):
+        self.assertEqual(calc(999, 999), 999 * 999)
 
-        def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+    def test_valid_input_middle(self):
+        self.assertEqual(calc(500, 500), 500 * 500)
 
-        def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+    # 無効同値 - 範囲外
+    def test_A_below_min(self):
+        self.assertEqual(calc(0, 500), -1)
 
+    def test_A_above_max(self):
+        self.assertEqual(calc(1000, 500), -1)
+
+    def test_B_below_min(self):
+        self.assertEqual(calc(500, 0), -1)
+
+    def test_B_above_max(self):
+        self.assertEqual(calc(500, 1000), -1)
+
+    # 無効同値 - 非整数入力
+    def test_A_is_float(self):
+        self.assertEqual(calc(1.5, 500), -1) 
+
+    def test_B_is_float(self):
+        self.assertEqual(calc(500, 2.3), -1)
+
+    def test_A_is_string(self):
+        self.assertEqual(calc('a', 500), -1)
+
+    def test_B_is_string(self):
+        self.assertEqual(calc(500, "xyz"), -1)
+
+    def test_A_is_special_character(self):
+        self.assertEqual(calc("#", 500), -1)
+
+    def test_B_is_special_character(self):
+        self.assertEqual(calc(500, "!"), -1)
+
+    def test_A_is_empty(self):
+        self.assertEqual(calc("", 500), -1)
+
+    def test_B_is_empty(self):
+        self.assertEqual(calc(500, ""), -1) 
+
+    def test_A_is_None(self):
+        self.assertEqual(calc(None, 500), -1) 
+
+    def test_B_is_None(self):
+        self.assertEqual(calc(500, None), -1) 
